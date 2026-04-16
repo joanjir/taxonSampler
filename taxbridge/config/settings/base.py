692634a5423d3c11d4f3fd4ffa3a5a0a4bda97b0
joanjir <joanjir@gmail.com>
2026-04-16 15:46:49 -0400

    feat: update requirements and enhance template structure with FontAwesome and Select2 integration

--- taxbridge/config/settings/base.py
+++ taxbridge/config/settings/base.py
@@ -68,6 +68,7 @@ except ImportError:
 # ======================
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "django.middleware.gzip.GZipMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
